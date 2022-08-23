fd971f0f4fb69c0fb8f84ab5fa5e6cd32f93512b
SaifHayek <seefhayek4@gmail.com>
2022-08-24 00:27:18 +0300

    modify dburl to suit development and production and deployment related #2

--- src/database/config/db_connection.js
+++ src/database/config/db_connection.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
 require('env2')('.env');
 
-const { DB_URL } = process.env;
-
+const { DB_URL, DEV_DB_URL, NODE_ENV } = process.env;
+let dbUrl = '';
+switch (NODE_ENV) {
+  case 'production': dbUrl = DB_URL; break;
+  case 'development': dbUrl = DEV_DB_URL; break;
+  default: throw new Error('No Database found !');
+}
 const connection = new Pool({
-  connectionString: DB_URL,
+  connectionString: dbUrl,
   ssl: new URL(DB_URL).hostname !== 'localhost',
 });
 
